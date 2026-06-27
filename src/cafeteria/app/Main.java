164fc50e307aae68d4c653ee02279f96076b1b1e
thamires ellen souza <thamiresellen@MacBook-Air-de-thamires.local>
2026-06-27 17:07:40 -0300

    feat: implementa classe ClienteVIP

--- src/cafeteria/app/Main.java
+++ src/cafeteria/app/Main.java
@@ -1,10 +1,22 @@
 package cafeteria.app;
 
+import cafeteria.modelo.Atendente;
+import cafeteria.modelo.Bebida;
+import cafeteria.modelo.Comida;
+import cafeteria.modelo.Produto;
+import cafeteria.modelo.ProdutoRepository;
+import cafeteria.modelo.Pedido;
+import cafeteria.modelo.ItemPedido;
+import cafeteria.modelo.Cliente;
+import cafeteria.modelo.ClienteVip;
+import cafeteria.modelo.ClienteStandard;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Bem-vindo ao sistema da Cafeteria Byte & Brew!");
+		
+		
+	}
 	}
 
-}
--- src/cafeteria/servico/Promocional.java
+++ src/cafeteria/servico/Promocional.java
@@ -2,4 +2,7 @@ package cafeteria.servico;
 
 public interface Promocional {
 
+	double aplicarDesconto(double totalAtual);
+		
+	
 }
