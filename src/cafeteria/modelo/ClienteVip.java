c2f93b88cc1693e5cc22639d27243ed5e9c492f8
bcaueLPS <30941831+bcaueLPS@users.noreply.github.com>
2026-06-27 16:25:16 -0300

    Update ClienteVip.java

--- src/cafeteria/modelo/ClienteVip.java
+++ src/cafeteria/modelo/ClienteVip.java
@@ -1,5 +1,31 @@
 package cafeteria.modelo;
 
-public class ClienteVip {
+public class ClienteVIP extends Cliente {
 
+	private static final int XP_REAL = 10;
+
+	public ClienteVIP(String nome, String cpf) {
+		super(nome, cpf);
+	}
+
+	@Override
+	public void adicionarXP(double valor) {
+		super.adicionarXP(valor * 2);
+	}
+
+	public boolean pagarXP(double valor) {
+
+		if (valor <= 0) {
+			return false;
+		}
+
+		int pontos = (int) Math.round(valor * XP_REAL);
+
+		if (getSaldoXP() < pontos) {
+			return false;
+		}
+
+		descontarXP(pontos);
+		return true;
+	}
 }
